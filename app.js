15567fea47d3a107f4d83f8679b151b9815f50e3
Pasquale <pasquale.coppola6@gmail.com>
2022-10-26 10:01:35 -0400

    updates to game library

--- app.js
+++ app.js
@@ -11,28 +11,28 @@ const library = [
     title: "Tzolk'in",
     players: "2-4",
     img: "./pictures/Tzolk'in.webp",
-    desc: "..."
+    desc: "The Mayan Calendar game?"
 },
 {
     id: 3,
     title: "Kanban EV",
     players: "2-4",
     img: "./pictures/KanbanEV.webp",
-    desc: "..."
+    desc: "A game about building cars"
 },
 {
     id: 4,
     title: "Lost Ruins of Arnak",
     players: "2-4",
     img: "./pictures/LostRuins.webp",
-    desc: "..."
+    desc: "Worker placement game about exploring in the jungle"
 },
 {
     id: 5,
     title: "Dominant Species",
     players: "2-6",
     img: "./pictures/Dominant.webp",
-    desc: "..."
+    desc: "One of the best games there ever was."
 }];
 
 const sectionCenter = document.querySelector(".section-center");
@@ -42,12 +42,12 @@ window.addEventListener('DOMContentLoaded', function() {
         //console.log(game);
         return `<article class="menu-item">
         <img src=${game.img} class="photo" alt=${game.title}>
-         <div class="item-info">
+         <div class="game-info">
             <header>
-            <h4${game.title}</h4>
+            <h4>${game.title}</h4>
             <h4 class="players">${game.players}</h4>
             </header>
-            <p class=${game.desc}>Good game</p>
+            <p class="game-desc">${game.desc}</p>
     </div>
     </article>`;
     });
